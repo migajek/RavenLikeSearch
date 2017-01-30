6bd5db66fb9190106b4b8c2489d08b31de2ae6ce
migajek <migajek@gmail.com>
2017-01-30 13:11:13 +0100

    pumping data from production db to demo db, comparing

--- RavenPlayground/Program.cs
+++ RavenPlayground/Program.cs
@@ -2,117 +2,146 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using Raven.Abstractions.Data;
 using Raven.Abstractions.Util;
 using Raven.Client;
+using Raven.Client.Document;
 using Raven.Client.Embedded;
+using Raven.Client.Linq;
 
 namespace RavenPlayground
 {
     public class Entity
     {
-        public string Id { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
-        public override bool Equals(object obj)
-        {
-            return (obj as Entity)?.Id == Id;
-        }
-                
-        public override int GetHashCode()
-        {
-            return Id?.GetHashCode() ?? 0;
-        }
+        public long? ArticleTemplateId { get; set; }        
     }
 
-    public static class EntExt
-    {
-        public static string Dump(this IQueryable<Entity> q)
-        {
-            return q.AsEnumerable().Dump();
-        }
-
-        public static string Dump(this IEnumerable<Entity> q)
-        {
-            return String.Join(", ", q.Select(x => x.Name));
-        }
-    }
-
+    
     class Program
     {
-        private static readonly IEnumerable<Entity> _entities = new[]
-        {
-            new Entity() {Id = "1", Name = "abc"},
-            new Entity() {Id = "2", Name = "def"},
-            new Entity() {Id = "3", Name = "abc def"},
-            new Entity() {Id = "4", Name = "def abc"},
-            new Entity() {Id = "5", Name = "zz def zz"},
-            new Entity() {Id = "6", Name = "zz abc def zz"}
-        };
-
-        private static string Compare(IQueryable<Entity> actual, IEnumerable<Entity> expected)
-        {
-            var actualEvaluated = actual.OrderBy(x => x.Id).ToArray().AsQueryable();
-            var expectedEvaluated = expected.OrderBy(x => x.Id).ToArray().AsQueryable();
-            if (actualEvaluated.SequenceEqual(expectedEvaluated))
-                return "COMPARE OK";
-
-            var extras = actualEvaluated.Where(x => !expectedEvaluated.Contains(x)).ToArray();
-            var missing = expectedEvaluated.Where(x => !actualEvaluated.Contains(x)).ToArray();
-            return
-                $"ACTUAL: {actualEvaluated.Dump()}\nEXPECTED: {expectedEvaluated.Dump()}\nDIFF: {String.Join(", ", extras.Select(x => "+" + x.Name))}, {String.Join(", ", missing.Select(x => "-" + x.Name))}";
-        }
-
+        
         static void Main(string[] args)
-        {            
-            const string dir = @"C:\tmp\rave";
-            if (Directory.Exists(dir))
-                Directory.Delete(dir, true);
-
-            var store = new EmbeddableDocumentStore
+        {                        
+            var sourceStore = new DocumentStore
             {
-                DataDirectory = dir,
-                RunInMemory = true,
-            };
-            store.Conventions.DefaultQueryingConsistency = Raven.Client.Document.ConsistencyOptions.AlwaysWaitForNonStaleResultsAsOfLastWrite;
-            store.Configuration.Storage.Voron.AllowOn32Bits = true;
-            store.Initialize();
-
-            using (var sess = store.OpenSession())
-            {
-                foreach (var entity in _entities)
+                Url = "http://localhost:8080/",
+                DefaultDatabase = "Tenant-10",
+                Conventions =
                 {
-                    sess.Store(entity);
-                }                
-                sess.SaveChanges();
-            }
+                    FindTypeTagName = type =>
+                    {
+                        if (type == typeof (Entity))
+                            return
+                                DocumentConvention.DefaultTransformTypeTagNameToDocumentKeyPrefix(
+                                    "Abax_Worker_Services_Documents_Projections_Article_Model_Article");
+                        return DocumentConvention.DefaultTypeTagName(type);
+                    }
+                },
+            };
+            sourceStore.Initialize();
 
-            using (var sess = store.OpenSession())
+            var outputStore = new DocumentStore
             {
-                Console.WriteLine("*abc*");
-                var e1 = Compare(sess.Query<Entity>()
-                    .Search(x => x.Name, "*abc*", escapeQueryOptions: EscapeQueryOptions.AllowAllWildcards),
-                    _entities.Where(x => x.Name.Contains("abc")));
-                Console.WriteLine(e1);
+                Url = "http://localhost:8080/",
+                DefaultDatabase = "demo-db",               
+            };
+            outputStore.Initialize();
+            outputStore.DatabaseCommands.GlobalAdmin.EnsureDatabaseExists("demo-db");
 
-                Console.WriteLine("*abc def* #1 - AllowAllWildcards");
-                var e2 = Compare(sess.Query<Entity>()
-                    .Search(x => x.Name, "*abc def*", escapeQueryOptions: EscapeQueryOptions.AllowAllWildcards),
-                    _entities.Where(x => x.Name.Contains("abc def")));
-                Console.WriteLine(e2);
 
-                Console.WriteLine("*abc def* #2 - Escape + RawQuery");
-                var escaped = RavenQuery.Escape("abc def");
-                var e3 = Compare(sess.Query<Entity>()
-                    .Search(x => x.Name, $"*{escaped}*", escapeQueryOptions: EscapeQueryOptions.RawQuery),
-                    _entities.Where(x => x.Name.Contains("abc def")));
-                Console.WriteLine(e3);
+            //using (var sess = sourceStore.OpenSession())
+            //{
+            //    var arts = sess.Query<Entity>().Take(1000).ToList();
+            //    arts.ForEach(art =>
+            //    {
+            //        using (var sess2 = outputStore.OpenSession())
+            //        {
+            //            sess2.Store(art);                        
+            //            sess2.SaveChanges();
+            //        }
+            //    });
+            //}
+            //Console.WriteLine("Pumping done");
 
-                Console.WriteLine("*abc def* #3 - Escape + Replace space + RawQuery");
-                escaped = RavenQuery.Escape("abc def").Replace(' ', '\\');
-                var e4 = Compare(sess.Query<Entity>()
-                    .Search(x => x.Name, $"*{escaped}*", escapeQueryOptions: EscapeQueryOptions.RawQuery),
-                    _entities.Where(x => x.Name.Contains("abc def")));
-                Console.WriteLine(e4);
+            Console.WriteLine("SOURCE: ");
+            using (var sess = sourceStore.OpenSession())
+            {
+                var whereServerSide = sess.Query<Entity>()
+                    .Where(x => x.Id.In(420, 406, 262, 263, 264))
+                    .Take(1000)
+                    .ToList();                    
+
+
+                Console.WriteLine($"Got {whereServerSide.Count} items - ravendb");
+
+                var whereInMemory = sess.Query<Entity>()
+                    .Take(1000)
+                    .ToList()
+                    .Where(x => x.Id.In(420, 406, 262, 263, 264))
+                    .ToList();
+                Console.WriteLine($"Got {whereInMemory.Count} items - in memory");
             }
+
+            Console.WriteLine("\n\nDEST: !!!");
+            using (var sess = outputStore.OpenSession())
+            {
+                var whereServerSide = sess.Query<Entity>()
+                    .Where(x => x.Id.In(420, 406, 262, 263, 264))
+                    .Take(1000)
+                    .ToList();
+
+
+                Console.WriteLine($"Got {whereServerSide.Count} items - ravendb");
+
+                var whereInMemory = sess.Query<Entity>()
+                    .Take(1000)
+                    .ToList()
+                    .Where(x => x.Id.In(420, 406, 262, 263, 264))
+                    .ToList();
+                Console.WriteLine($"Got {whereInMemory.Count} items - in memory");
+            }
+
+
+            //using (var sess = store.OpenSession())
+            //{
+            //    if (sess.Query<Entity>().Count() < 1000)
+            //        Enumerable.Range(1, 1024)
+            //            .Select(x => new Entity()
+            //            {
+            //                ArticleTemplateId = x < 850 ? x + 200 : (long?) null,
+            //                Id = x+10,
+            //                Name = $"Article #{x}"
+            //            })
+            //            .ToList()
+            //            .ForEach(x => sess.Store(x));
+            //    sess.SaveChanges();
+            //}
+
+            //using (var sess = store.OpenSession())
+            //{
+            //    sess.Advanced.MaxNumberOfRequestsPerSession = 1000;
+            //    //var ar = sess.Query<Entity>().Where(x => x.Id.In(262, 263, 264)).ToArray();
+            //    var chunks = Enumerable.Range(1, 1024)
+            //        .Select(x => x + 10)
+            //        .Select((x, i) => new {Index = i, Value = (long)x})
+            //        .GroupBy(x => x.Index/3)
+            //        .Select(x => x.Select(v => v.Value).ToList());
+
+            //    foreach (var chunk in chunks)
+            //    {
+            //        var ar = sess.Query<Entity>().Where(x => x.Id.In(chunk)).ToArray();
+            //        var missings = chunk.Where(idx => ar.All(ent => ent.Id != idx));
+            //        foreach (var missing in missings)
+            //        {
+            //            Console.WriteLine($"Missing #{missing}");
+            //        }                    
+            //    }
+            //    Console.WriteLine("Done");
+
+            //}
 
             Console.ReadLine();
         }
